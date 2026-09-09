4e360ab9d0abb1c244443cc6959252305944a955
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 11:05:25 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+window.TRAVEL_CONFIG={
+  DEFAULT_COUNTRY:'IN',
+  API_URL:'',
+  ADMIN_URL:'',
+  REST_COUNTRIES_BASE:'https://restcountries.com/v3.1',
+  OPEN_METEO_FORECAST:'https://api.open-meteo.com/v1/forecast',
+  FX_RATE_BASE:'https://api.frankfurter.dev/v2/rate',
+  DEFAULT_PROFILE:{passportCountry:'IN',homeCurrency:'INR',travelStyle:'Mid-range',interests:['Culture','Nature','Food']}
+};
