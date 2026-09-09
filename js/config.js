6c560e5be4caa64f2f2895ee853658d27e6e732c
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 10:44:46 -0700

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,19 @@
+window.TRAVEL_CONFIG = {
+  DEFAULT_COUNTRY: 'IN',
+
+  // Paste your deployed Apps Script /exec URL here after deployment.
+  API_URL: '',
+
+  REST_COUNTRIES_BASE: 'https://restcountries.com/v3.1',
+  OPEN_METEO_FORECAST: 'https://api.open-meteo.com/v1/forecast',
+  FX_RATE_BASE: 'https://api.frankfurter.dev/v2/rate',
+
+  DEFAULT_PROFILE: {
+    passportCountry: 'IN',
+    homeCurrency: 'INR',
+    travelStyle: 'Mid-range',
+    interests: ['Culture','Nature','Food']
+  },
+
+  ADMIN_URL: ''
+};
