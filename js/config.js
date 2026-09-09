c5b94bc221aa616c7c71e53729c29b00b8624467
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 11:12:34 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window.TRAVEL_CONFIG = {
   DEFAULT_COUNTRY: 'IN',
 
   // Paste your deployed Apps Script /exec URL here after deployment.
-  API_URL: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbwwqTU79vOXB1-0ZKToNxDV1dwruDdu2W0vkWnMDiDrKsGXB1dBuEu5d3Dzq-Z4nPTEQw/exec',
 
   REST_COUNTRIES_BASE: 'https://restcountries.com/v3.1',
   OPEN_METEO_FORECAST: 'https://api.open-meteo.com/v1/forecast',
@@ -15,5 +15,5 @@ window.TRAVEL_CONFIG = {
     interests: ['Culture','Nature','Food']
   },
 
-  ADMIN_URL: ''
+  ADMIN_URL: 'https://script.google.com/macros/s/AKfycbwwqTU79vOXB1-0ZKToNxDV1dwruDdu2W0vkWnMDiDrKsGXB1dBuEu5d3Dzq-Z4nPTEQw/exec'
 };
