3356b8ac8c4f3dcc04042c22a8aa244b970978bb
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-09 11:09:48 -0700

    Delete js directory

--- js/config.js
+++
@@ -1,9 +0,0 @@
-window.TRAVEL_CONFIG={
-  DEFAULT_COUNTRY:'IN',
-  API_URL:'https://script.google.com/macros/s/AKfycbxgwRnieoT7LFA0FWyd-PKvsr3nvElDdM1eyUfqvuUGZbxH29HUzwrScxVTXh1fEKNYDw/exec',
-  ADMIN_URL:'https://script.google.com/macros/s/AKfycbxgwRnieoT7LFA0FWyd-PKvsr3nvElDdM1eyUfqvuUGZbxH29HUzwrScxVTXh1fEKNYDw/exec',
-  REST_COUNTRIES_BASE:'https://restcountries.com/v3.1',
-  OPEN_METEO_FORECAST:'https://api.open-meteo.com/v1/forecast',
-  FX_RATE_BASE:'https://api.frankfurter.dev/v2/rate',
-  DEFAULT_PROFILE:{passportCountry:'IN',homeCurrency:'INR',travelStyle:'Mid-range',interests:['Culture','Nature','Food']}
-};
