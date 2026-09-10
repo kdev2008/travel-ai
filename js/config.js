88baedf5235688378550a929a1d2ddd27e10cd6c
kdev2008 <90689851+kdev2008@users.noreply.github.com>
2026-09-10 08:44:46 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,8 +4,8 @@
 window.TRAVEL_CONFIG = {
 
   /* REQUIRED after Apps Script deployment. Paste the SAME /exec URL into both. */
-  API_URL: '',
-  ADMIN_URL: '',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxEdENc1A-xGuKGrJa403ERwfb0AAQmQdmKW4nOHvjHDHmJxe3OB7i7RIlJM9ZT3WXo4A/exec',
+  ADMIN_URL: 'https://script.google.com/macros/s/AKfycbxEdENc1A-xGuKGrJa403ERwfb0AAQmQdmKW4nOHvjHDHmJxe3OB7i7RIlJM9ZT3WXo4A/exec',
 
   /* Country facts and the India boundary are embedded in js/app.js.
      There is no country API to go down, rate-limit or start charging. */
